800c9b2599374fb3a834ed6ad28b1a30d8993d02
@zacbe <zacbegonzalez@gmail.com>
2024-05-05 18:41:33 +0200

    chore: Update contactClient to use axios for fetching contacts

--- orders-service/src/libs/contactClient.ts
+++ orders-service/src/libs/contactClient.ts
@@ -1,24 +1,14 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 const getContacts = async (id: string): Promise<User | null> => {
   const host = process.env.CONTACTS_SERVICE_HOST || 'localhost';
   const port = process.env.CONTACTS_SERVICE_PORT || '8080';
   const path = `api/v1/person/${id}`;
-  const baseUrl = `http://${host}:${port}`;
+  const url = `http://${host}:${port}/${path}`;
 
   try {
-    const response = await fetch(`${baseUrl}/${path}`);
-    const status = response.status;
-    console.log(`Status code: ${status}`);
-
-    if (!response.ok) {
-      const errorResponse: any = await response.json();
-      const message = errorResponse.message || 'Error fetching contacts';
-      console.error(message);
-      return null;
-    }
-    const data = await response.json();
-    return data as User;
+    const response = await axios.get<User>(url);
+    return response.data;
   } catch (e: any) {
     throw new Error(e);
   }
