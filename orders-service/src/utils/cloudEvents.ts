b633b56010518a383b07a1ecdd4a95e9047da0cc
@zacbe <zacbegonzalez@gmail.com>
2024-05-06 11:31:19 +0200

    chore: Add cloudevents and kafkajs dependencies, update envConfig

---
+++ orders-service/src/utils/cloudEvents.ts
@@ -0,0 +1,20 @@
+import { CloudEvent } from 'cloudevents';
+
+import { env } from './envConfig';
+
+function composeCloudEvent(type: string, data: any) {
+  const source = env.EVENT_SOURCE;
+  const ce = new CloudEvent({ type, source, data, datacontenttype: 'application/json' });
+  return ce;
+}
+
+const orderCreatedEvent = (data: any): CloudEvent => composeCloudEvent('order.created', data);
+const orderUpdatedEvent = (data: any): CloudEvent => composeCloudEvent('order.updated', data);
+const orderDeletedEvent = (data: any): CloudEvent => composeCloudEvent('order.deleted', data);
+
+function readCloudEvent(event: any) {
+  console.log(event);
+  return event.data;
+}
+
+export { orderCreatedEvent, orderDeletedEvent, orderUpdatedEvent, readCloudEvent };
--- orders-service/src/utils/envConfig.ts
+++ orders-service/src/utils/envConfig.ts
@@ -8,5 +8,6 @@ export const env = cleanEnv(process.env, {
   HOST: host({ devDefault: testOnly('localhost') }),
   PORT: port({ devDefault: testOnly(8081) }),
   CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:8081') }),
-  CONTACTS_SERVICE_HOST: str({ devDefault: testOnly('http://contact-service:8080') }),
+  CONTACTS_SERVICE_HOST: str({ devDefault: testOnly('http://localhost:8080') }),
+  EVENT_SOURCE: str({ devDefault: testOnly('http://localhost:8081/v1/api/orders') }),
 });
